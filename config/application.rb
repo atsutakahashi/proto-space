dc3c093a072b7008a96a33f041b6a90bdf09f178
drumnaoto <drumnaoto@gmail.com>
2017-11-15 12:43:42 +0900

    error
     !     Precompiling assets failed.
    というエラー対応の為。

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Miuraspace
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = true
     config.active_record.raise_in_transactional_callbacks = true
     config.i18n.default_locale = :ja
     config.generators do |g|
