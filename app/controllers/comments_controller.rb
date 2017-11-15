26069b4a08440d0aa00ebb1f82a047eb9dd934ff
drumnaoto <drumnaoto@gmail.com>
2017-11-15 11:54:02 +0900

    add-respond-at-comment_contorlloer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
 
- def create
-   @comment = Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
- end
+  def create
+    @comment = Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:prototype_id]) }
+      format.json
+    end
+  end
 
- private
- def comment_params
-   params.require(:comment).permit(:text).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
- end
+  private
+    def comment_params
+     params.require(:comment).permit(:text).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
+    end
 end
