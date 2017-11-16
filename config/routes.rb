fe0850c6b83f1980d6b8b9131f9669c7b8a6532a
drumnaoto <drumnaoto@gmail.com>
2017-11-16 09:35:30 +0900

    add-like-function

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
   resources :users, only: [:show, :edit, :update,]
+  resources :likes, omly: [:create, :destroy]
 end
