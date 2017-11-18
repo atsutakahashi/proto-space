cb32513dad4ca99c1b50a7a1d9bc74a8128a0b99
入江夏輝 <natsuki.irie@irienatsukinoMacBook-Pro.local>
2017-11-18 11:24:58 +0900

    destroy-action edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
+
   private
     def comment_params
      params.require(:comment).permit(:text).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
