8cb7de6d82c6900a1ec6340074989856d1efded2
入江夏輝 <natsuki.irie@irienatsukinoMacBook-Pro.local>
2017-11-21 11:11:43 +0900

    create newest-routes,controller
    ルーティングにnewestパス追加
    コントローラーにorderメソッドにより、newest,popular画面切り替え実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-
+  # get 'prototypes' => 'prototypes#newest'
   resources :prototypes, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
+    collection do
+      get 'newest'
+    end
     resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
   resources :users, only: [:show, :edit, :update,]
