e1a50c15d9e8718e0aff55e9b0ec2603057d2f9e
入江夏輝 <natsuki.irie@irienatsukinoMacBook-Pro.local>
2017-11-23 18:15:11 +0900

    sub-prototype-view3

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -22,24 +22,66 @@ $(document).on('turbolinks:load', function(){
 });
 
 $(document).on('turbolinks:load', function(){
-    $('#sub-image').change(function(){
+    $('.0').change(function(){
         if (!this.files.length) {
             return;
         }
         var file = this.files[0],           //画像１つのみ選択
-            image = $('#sub_image_uploader'),
+            image = $('.0'),
             fileReader = new FileReader();
 
         // 読み込みが完了した際のイベントハンドラ。imgのsrcにデータセット
         fileReader.onload = function(event) {
             // 読み込んだデータをimgに設定
-            image.children('img.sub').attr('src', event.target.result);
+            image.children('img.0').attr('src', event.target.result);
             // imgLiquid - imgの親要素に指定
             image.imgLiquid({fill: false});
         };
 
         // 画像読み込み
         fileReader.readAsDataURL(file);
+    });
+});
 
+$(document).on('turbolinks:load', function(){
+    $('.1').change(function(){
+        if (!this.files.length) {
+            return;
+        }
+        var file = this.files[0],           //画像１つのみ選択
+            image = $('.1'),
+            fileReader = new FileReader();
+
+        // 読み込みが完了した際のイベントハンドラ。imgのsrcにデータセット
+        fileReader.onload = function(event) {
+            // 読み込んだデータをimgに設定
+            image.children('img.1').attr('src', event.target.result);
+            // imgLiquid - imgの親要素に指定
+            image.imgLiquid({fill: false});
+        };
+
+        // 画像読み込み
+        fileReader.readAsDataURL(file);
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $('.2').change(function(){
+        if (!this.files.length) {
+            return;
+        }
+        var file = this.files[0],           //画像１つのみ選択
+            image = $('.2'),
+            fileReader = new FileReader();
+
+        // 読み込みが完了した際のイベントハンドラ。imgのsrcにデータセット
+        fileReader.onload = function(event) {
+            // 読み込んだデータをimgに設定
+            image.children('img.2').attr('src', event.target.result);
+            // imgLiquid - imgの親要素に指定
+            image.imgLiquid({fill: false});
+        };
+
+        // 画像読み込み
+        fileReader.readAsDataURL(file);
     });
 });
