4fa22a905d23bc93891edbce93586e2095e5843e
drumnaoto <drumnaoto@gmail.com>
2017-11-24 14:52:29 +0900

    edit-like.js
    微調整。

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -8,7 +8,6 @@ $(document).on('turbolinks:load', function () {
   });
 });
 function like_function(id, button, heart, sum) {
-
   if (button.hasClass("decrement")){
     $.ajax({
       url: /likes/+id,
@@ -20,15 +19,15 @@ function like_function(id, button, heart, sum) {
     .done(function(data){
       button.removeClass("decrement").addClass("increment");
       heart.attr("src", "/assets/icon_heart.ico");
-      $("#like-sum").text(sum-1);
-    })
+      $("#like-sum").text(Number(sum-1));
+    });
   }else{
     $.ajax({
       url: /likes/,
       type: "post",
       data: {prototype_id: id},
       dataType: "json",
-    })
+    });
 
     .done(function(data){
       button.removeClass("increment").addClass("decrement");
